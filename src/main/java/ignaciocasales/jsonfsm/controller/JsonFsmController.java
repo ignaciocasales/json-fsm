ec55c8775ff97a83b0eb838231a400cb69efd8b3
ignaciocasales <casales.ignacio@gmail.com>
2022-06-22 13:35:07 -0300

    Add service

--- src/main/java/ignaciocasales/jsonfsm/controller/JsonFsmController.java
+++ src/main/java/ignaciocasales/jsonfsm/controller/JsonFsmController.java
@@ -1,48 +1,35 @@
 package ignaciocasales.jsonfsm.controller;
 
-import ignaciocasales.jsonfsm.JsonFsm;
 import ignaciocasales.jsonfsm.automata.IllegalStateTransitionException;
-import ignaciocasales.jsonfsm.automata.State;
 import ignaciocasales.jsonfsm.dto.Response;
 import ignaciocasales.jsonfsm.mapper.ResponseMapper;
+import ignaciocasales.jsonfsm.service.FiniteAutomata;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Objects;
 import java.util.Optional;
 import java.util.regex.Pattern;
 
 @Slf4j
 @RestController
+@RequiredArgsConstructor
 public class JsonFsmController {
     private static final String EMPTY_STRING = "";
     private static final Pattern WHITESPACE_PATTERN = Pattern.compile("\\s");
+    private final FiniteAutomata finiteAutomata;
 
     @PostMapping("/test")
     public Response method(@RequestBody final String input) {
-        return process(sanitize(input));
+        return finiteAutomata.start(sanitize(input));
     }
 
     @ExceptionHandler({IllegalStateTransitionException.class})
     public Response handleException(final IllegalStateTransitionException ex) {
         return ResponseMapper.invalid(ex.getCharacter(), ex.getIndex());
-    }
-
-    private static Response process(final String input) {
-        State state = JsonFsm.parentObject();
-        for (int i = 0; i < input.length(); i++) {
-            final String c = String.valueOf(input.charAt(i));
-            final String index = String.valueOf(i);
-            state = state.getTransitions().stream()
-                    .map(transition -> transition.apply(c))
-                    .filter(Objects::nonNull)
-                    .findAny()
-                    .orElseThrow(() -> new IllegalStateTransitionException(c, index));
-        }
-        return state.isFinal() ? ResponseMapper.valid() : ResponseMapper.invalid();
     }
 
     private static String sanitize(final String input) {
---
+++ src/main/java/ignaciocasales/jsonfsm/service/FiniteAutomata.java
@@ -0,0 +1,27 @@
+package ignaciocasales.jsonfsm.service;
+
+import ignaciocasales.jsonfsm.JsonFsm;
+import ignaciocasales.jsonfsm.automata.IllegalStateTransitionException;
+import ignaciocasales.jsonfsm.automata.State;
+import ignaciocasales.jsonfsm.dto.Response;
+import ignaciocasales.jsonfsm.mapper.ResponseMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+
+@Service
+public class FiniteAutomata {
+    public Response start(final String input) {
+        State state = JsonFsm.parentObject();
+        for (int i = 0; i < input.length(); i++) {
+            final String c = String.valueOf(input.charAt(i));
+            final String index = String.valueOf(i);
+            state = state.getTransitions().stream()
+                    .map(transition -> transition.apply(c))
+                    .filter(Objects::nonNull)
+                    .findAny()
+                    .orElseThrow(() -> new IllegalStateTransitionException(c, index));
+        }
+        return state.isFinal() ? ResponseMapper.valid() : ResponseMapper.invalid();
+    }
+}
